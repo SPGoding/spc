095418075b87e5967ec06f4e216445b9af0231df
SPGoding <SPGoding@outlook.com>
2018-07-09 21:54:28 +0800

    :sparkles:Add some special cases.

--- tool/SpcTool/Program.cs
+++ tool/SpcTool/Program.cs
@@ -80,6 +80,15 @@ namespace SpcTool
                 Jar1Path = args[2],
                 Jar2Path = args[3]
             };
+            
+            if (arg.Jar1Path.ElementAt(arg.Jar1Path.Length - 1) != '\\')
+            {
+                arg.Jar1Path += @"\";
+            }
+            if (arg.Jar2Path.ElementAt(arg.Jar2Path.Length - 1) != '\\')
+            {
+                arg.Jar2Path += @"\";
+            }
 
             if (args.Length == 4)
             {
@@ -155,7 +164,14 @@ namespace SpcTool
                 else
                 {
                     dict[md5].Add(name);
-                    Console.WriteLine($"Same MD5: \"{name}\".");
+
+                    // DELETE:
+                    if (dict[md5].Count == 2)
+                    {
+                        Console.WriteLine("Same MD5");
+                        Console.WriteLine(dict[md5][0]);
+                    }
+                    Console.WriteLine(name);
                 }
             }
             foreach (var dir in Directory.GetDirectories(path))
@@ -254,6 +270,70 @@ namespace SpcTool
                 File.WriteAllText(Path.Combine(outputPath, "source.ts"), ts);
             }
         }
+
+        private static readonly Dictionary<string, string> specialCases =  new Dictionary<string, string>() {
+            { "assets/minecraft/textures/blocks/beetroots_stage_0.png", "assets/minecraft/textures/block/beetroots_stage0.png" },
+            { "assets/minecraft/textures/blocks/carrots_stage_0.png", "assets/minecraft/textures/block/carrots_stage0.png" },
+            { "assets/minecraft/textures/blocks/beetroots_stage_1.png", "assets/minecraft/textures/block/beetroots_stage1.png" },
+            { "assets/minecraft/textures/blocks/carrots_stage_1.png", "assets/minecraft/textures/block/carrots_stage1.png" },
+            { "assets/minecraft/textures/blocks/beetroots_stage_2.png", "assets/minecraft/textures/block/beetroots_stage2.png" },
+            { "assets/minecraft/textures/blocks/carrots_stage_2.png", "assets/minecraft/textures/block/carrots_stage2.png" },
+            { "assets/minecraft/textures/blocks/chain_command_block_back.png.mcmeta", "assets/minecraft/textures/block/chain_command_block_back.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/chain_command_block_conditional.png.mcmeta", "assets/minecraft/textures/block/chain_command_block_conditional.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/chain_command_block_front.png.mcmeta", "assets/minecraft/textures/block/chain_command_block_front.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/chain_command_block_side.png.mcmeta", "assets/minecraft/textures/block/chain_command_block_side.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/command_block_back.png.mcmeta", "assets/minecraft/textures/block/command_block_back.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/command_block_conditional.png.mcmeta", "assets/minecraft/textures/block/command_block_conditional.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/command_block_front.png.mcmeta", "assets/minecraft/textures/block/command_block_front.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/command_block_side.png.mcmeta", "assets/minecraft/textures/block/command_block_side.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/cauldron_inner.png", "assets/minecraft/textures/block/cauldron_inner.png" },
+            { "assets/minecraft/textures/blocks/hopper_inside.png", "assets/minecraft/textures/block/hopper_inside.png" },
+            { "assets/minecraft/textures/blocks/frosted_ice_0.png", "assets/minecraft/textures/block/frosted_ice_0.png" },
+            { "assets/minecraft/textures/blocks/ice.png", "assets/minecraft/textures/block/ice.png" },
+            { "assets/minecraft/textures/blocks/leaves_big_oak.png", "assets/minecraft/textures/block/dark_oak_leaves.png" },
+            { "assets/minecraft/textures/blocks/leaves_birch.png", "assets/minecraft/textures/block/birch_leaves.png" },
+            { "assets/minecraft/textures/blocks/leaves_oak.png", "assets/minecraft/textures/block/oak_leaves.png" },
+            { "assets/minecraft/textures/blocks/jukebox_side.png", "assets/minecraft/textures/block/jukebox_side.png" },
+            { "assets/minecraft/textures/blocks/noteblock.png", "assets/minecraft/textures/block/note_block.png" },
+            { "assets/minecraft/textures/blocks/furnace_top.png", "assets/minecraft/textures/block/furnace_top.png" },
+            { "assets/minecraft/textures/blocks/piston_bottom.png", "assets/minecraft/textures/block/piston_bottom.png" },
+            { "assets/minecraft/textures/blocks/fire_layer_1.png.mcmeta", "assets/minecraft/textures/block/fire_1.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/portal.png.mcmeta", "assets/minecraft/textures/block/nether_portal.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/potatoes_stage_0.png", "assets/minecraft/textures/block/potatoes_stage0.png" },
+            { "assets/minecraft/textures/blocks/potatoes_stage_1.png", "assets/minecraft/textures/block/potatoes_stage1.png" },
+            { "assets/minecraft/textures/blocks/potatoes_stage_2.png", "assets/minecraft/textures/block/potatoes_stage2.png" },
+            { "assets/minecraft/textures/blocks/melon_stem_connected.png", "assets/minecraft/textures/block/attached_melon_stem.png" },
+            { "assets/minecraft/textures/blocks/pumpkin_stem_connected.png", "assets/minecraft/textures/block/attached_pumpkin_stem.png" },
+            { "assets/minecraft/textures/blocks/melon_stem_disconnected.png", "assets/minecraft/textures/block/melon_stem.png" },
+            { "assets/minecraft/textures/blocks/pumpkin_stem_disconnected.png", "assets/minecraft/textures/block/pumpkin_stem.png" },
+            { "assets/minecraft/textures/blocks/quartz_block_side.png", "assets/minecraft/textures/block/quartz_block_side.png" },
+            { "assets/minecraft/textures/blocks/quartz_block_top.png", "assets/minecraft/textures/block/quartz_block_top.png" },
+            { "assets/minecraft/textures/blocks/repeating_command_block_back.png.mcmeta", "assets/minecraft/textures/block/repeating_command_block_back.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/repeating_command_block_conditional.png.mcmeta", "assets/minecraft/textures/block/repeating_command_block_conditional.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/repeating_command_block_front.png.mcmeta", "assets/minecraft/textures/block/repeating_command_block_front.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/repeating_command_block_side.png.mcmeta", "assets/minecraft/textures/block/repeating_command_block_side.png.mcmeta" },
+            { "assets/minecraft/textures/blocks/water_flow.png.mcmeta", "assets/minecraft/textures/block/water_flow.png.mcmeta" },
+            { "assets/minecraft/textures/entity/projectiles/arrow.png", "assets/minecraft/textures/entity/projectiles/arrow.png" },
+            { "assets/minecraft/textures/entity/projectiles/tipped_arrow.png", "assets/minecraft/textures/entity/projectiles/tipped_arrow.png" },
+            { "assets/minecraft/textures/blocks/sandstone_top.png", "assets/minecraft/textures/block/sandstone_top.png" },
+            { "assets/minecraft/textures/gui/advancements/backgrounds/adventure.png", "assets/minecraft/textures/gui/advancements/backgrounds/adventure.png" },
+            { "assets/minecraft/textures/blocks/end_stone.png", "assets/minecraft/textures/block/end_stone.png" },
+            { "assets/minecraft/textures/gui/advancements/backgrounds/end.png", "assets/minecraft/textures/gui/advancements/backgrounds/end.png" },
+            { "assets/minecraft/textures/blocks/farmland_dry.png", "assets/minecraft/textures/block/farmland.png" },
+            { "assets/minecraft/textures/gui/advancements/backgrounds/husbandry.png", "assets/minecraft/textures/gui/advancements/backgrounds/husbandry.png" },
+            { "assets/minecraft/textures/items/compass_07.png", "assets/minecraft/textures/item/compass_07.png" },
+            { "assets/minecraft/textures/items/compass_08.png", "assets/minecraft/textures/item/compass_08.png" },
+            { "assets/minecraft/textures/items/compass_09.png", "assets/minecraft/textures/item/compass_09.png" },
+            { "assets/minecraft/textures/items/compass_23.png", "assets/minecraft/textures/item/compass_23.png" },
+            { "assets/minecraft/textures/items/compass_24.png", "assets/minecraft/textures/item/compass_24.png" },
+            { "assets/minecraft/textures/items/compass_25.png", "assets/minecraft/textures/item/compass_25.png" },
+            { "assets/minecraft/textures/items/potion_bottle_drinkable.png", "assets/minecraft/textures/item/potion.png" },
+            { "assets/minecraft/textures/items/potion_bottle_empty.png", "assets/minecraft/textures/item/glass_bottle.png" },
+            { "assets/minecraft/textures/misc/unknown_pack.png", "assets/minecraft/textures/misc/unknown_pack.png" },
+            { "assets/minecraft/textures/misc/unknown_server.png", "assets/minecraft/textures/misc/unknown_server.png" },
+            { "assets/minecraft/textures/misc/pumpkinblur.png.mcmeta", "assets/minecraft/textures/misc/pumpkinblur.png.mcmeta" },
+            { "assets/minecraft/textures/misc/vignette.png.mcmeta", "assets/minecraft/textures/misc/vignette.png.mcmeta" },
+        };
     }
 
     struct Config
